a3a65c4f6f4efaa7d9a80e94bc124f33e58567c6
Eumir Gaspar <eumirgaspar@gmail.com>
2020-01-31 12:29:48 +0800

    Fixed bug when collection is empty

--- spec/pagy_cursor/uuid_cursor_spec.rb
+++ spec/pagy_cursor/uuid_cursor_spec.rb
@@ -4,52 +4,62 @@ require "pagy_cursor/pagy/extras/uuid_cursor"
 RSpec.describe PagyCursor do
   let(:backend) { TestController.new }
 
-  before do
-    Post.destroy_all
-    1.upto(100) do |i|
-      Post.create!(title: "post#{i}", created_at: (100-i).minutes.ago)
+  context 'no records' do
+    it 'returns an empty collection' do
+      pagy, records = backend.send(:pagy_uuid_cursor, Post.all)
+      expect(records).to be_empty
+      expect(pagy.has_more?).to eq(false)
     end
   end
 
-  it "paginates with defaults" do
-    pagy, records = backend.send(:pagy_uuid_cursor, Post.all)
-    expect(records.map(&:title)).to eq(
-      ["post100", "post99", "post98", "post97", "post96",
-        "post95", "post94", "post93", "post92", "post91",
-        "post90", "post89", "post88", "post87", "post86",
-        "post85", "post84", "post83", "post82", "post81"])
-    expect(pagy.has_more?).to eq(true)
-  end
+  context 'with records' do
+    before do
+      Post.destroy_all
+      1.upto(100) do |i|
+        Post.create!(title: "post#{i}", created_at: (100-i).minutes.ago)
+      end
+    end
 
-  it "paginates with before" do
-    record = Post.find_by! title: "post30"
-    pagy, records = backend.send(:pagy_uuid_cursor, Post.all, before: record.id)
-    expect(records.first.title).to eq("post29")
-    expect(records.last.title).to eq("post10")
-    expect(pagy.has_more?).to eq(true)
-  end
+    it "paginates with defaults" do
+      pagy, records = backend.send(:pagy_uuid_cursor, Post.all)
+      expect(records.map(&:title)).to eq(
+        ["post100", "post99", "post98", "post97", "post96",
+         "post95", "post94", "post93", "post92", "post91",
+         "post90", "post89", "post88", "post87", "post86",
+         "post85", "post84", "post83", "post82", "post81"])
+      expect(pagy.has_more?).to eq(true)
+    end
 
-  it "paginates with before nearly starting" do
-    record = Post.find_by! title: "post5"
-    pagy, records = backend.send(:pagy_uuid_cursor, Post.all, before: record.id)
-    expect(records.first.title).to eq("post4")
-    expect(records.last.title).to eq("post1")
-    expect(pagy.has_more?).to eq(false)
-  end
+    it "paginates with before" do
+      record = Post.find_by! title: "post30"
+      pagy, records = backend.send(:pagy_uuid_cursor, Post.all, before: record.id)
+      expect(records.first.title).to eq("post29")
+      expect(records.last.title).to eq("post10")
+      expect(pagy.has_more?).to eq(true)
+    end
 
-  it "paginates with after" do
-    record = Post.find_by! title: "post30"
-    pagy, records = backend.send(:pagy_uuid_cursor, Post.all, after: record.id)
-    expect(records.first.title).to eq("post31")
-    expect(records.last.title).to eq("post50")
-    expect(pagy.has_more?).to eq(true)
-  end
+    it "paginates with before nearly starting" do
+      record = Post.find_by! title: "post5"
+      pagy, records = backend.send(:pagy_uuid_cursor, Post.all, before: record.id)
+      expect(records.first.title).to eq("post4")
+      expect(records.last.title).to eq("post1")
+      expect(pagy.has_more?).to eq(false)
+    end
 
-  it "paginates with before nearly starting" do
-    record = Post.find_by! title: "post90"
-    pagy, records = backend.send(:pagy_uuid_cursor, Post.all, after: record.id)
-    expect(records.first.title).to eq("post91")
-    expect(records.last.title).to eq("post100")
-    expect(pagy.has_more?).to eq(false)
+    it "paginates with after" do
+      record = Post.find_by! title: "post30"
+      pagy, records = backend.send(:pagy_uuid_cursor, Post.all, after: record.id)
+      expect(records.first.title).to eq("post31")
+      expect(records.last.title).to eq("post50")
+      expect(pagy.has_more?).to eq(true)
+    end
+
+    it "paginates with before nearly starting" do
+      record = Post.find_by! title: "post90"
+      pagy, records = backend.send(:pagy_uuid_cursor, Post.all, after: record.id)
+      expect(records.first.title).to eq("post91")
+      expect(records.last.title).to eq("post100")
+      expect(pagy.has_more?).to eq(false)
+    end
   end
 end
