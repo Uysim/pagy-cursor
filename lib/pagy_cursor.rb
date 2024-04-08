4e57eaf796c589b0540c28852784e92b799e70b4
Jun Jiang <jasl9187@hotmail.com>
2024-04-08 15:48:54 +0800

    Add back version.rb

--- lib/pagy_cursor.rb
+++ lib/pagy_cursor.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 require "pagy"
 require "pagy_cursor/pagy/cursor"
 require "pagy_cursor/pagy/extras/cursor"
 require "pagy_cursor/pagy/extras/uuid_cursor"
 
+require "pagy_cursor/version"
+
 module PagyCursor
-  VERSION = "0.7.0"
 end
--- pagy_cursor.gemspec
+++ pagy_cursor.gemspec
@@ -1,7 +1,6 @@
-
 lib = File.expand_path("../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "pagy_cursor"
+require "pagy_cursor/version"
 
 Gem::Specification.new do |spec|
   spec.name          = "pagy_cursor"
